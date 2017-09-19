3fc7145532c039fadb36b653a6996b5f9a0b18e9
Tobias Kiertscher <kiertscher@th-brandenburg.de>
2017-09-19 12:17:15 +0200

    added Waiter for waiting for a target condition with a timeout

---
+++ src/LibraryTests/Waiter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mastersign.Tasks.Test
+{
+    static class Waiter
+    {
+        public static void WaitFor(Func<bool> criterion, int timeout)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            while (!criterion())
+            {
+                if (sw.ElapsedMilliseconds > timeout)
+                {
+                    throw new TimeoutException();
+                }
+                Thread.Sleep(10);
+            }
+        }
+    }
+}

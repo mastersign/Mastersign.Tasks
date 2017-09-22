235f0d6f90eacf759bce82f9c011ba9e00e8d639
Tobias Kiertscher <dev@mastersign.de>
2017-09-22 11:29:14 +0200

    included current state into TaskEventArgs

--- src/Library/TaskEventArgs.cs
+++ src/Library/TaskEventArgs.cs
@@ -8,9 +8,12 @@ namespace Mastersign.Tasks
     {
         public ITask Task { get; private set; }
 
+        public TaskState State { get; private set; }
+
         public TaskEventArgs(ITask task)
         {
             Task = task;
+            State = task.State;
         }
     }
 }

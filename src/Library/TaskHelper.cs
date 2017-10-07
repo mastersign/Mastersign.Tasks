6809f7a563580f4a3b753597e596b8bc9ef268c8
Tobias Kiertscher <dev@mastersign.de>
2017-10-07 17:04:31 +0200

    added TaskHelper with method for discovering responsibilities

---
+++ src/Library/TaskHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mastersign.Tasks
+{
+    public static class TaskHelper
+    {
+        public static IEnumerable<ITask> Responsibilities(ITask task, IEnumerable<ITask> allTasks)
+        {
+            foreach (var t in allTasks)
+            {
+                if (t == task) continue;
+                foreach (var d in t.Dependencies)
+                {
+                    if (d == task)
+                    {
+                        yield return t;
+                    }
+                }
+            }
+        }
+    }
+}

93dbef5bf280111e8f20ac5f3e6d2920447e3858
Tobias Kiertscher <kiertscher@th-brandenburg.de>
2017-10-10 15:05:49 +0200

    added script for running tests in a loop for detecting improbable concurrency errors

---
+++ auto/Run-Tests.ps1
@@ -0,0 +1,67 @@
+param (
+    $Cycles = 100,
+    $Configuration = "Debug"
+)
+
+$myDir = [IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition)
+$binDir = "$myDir\..\bin\$Configuration"
+$tmpDir = "$myDir\..\testlogs"
+
+if (Test-Path $tmpDir)
+{
+    del $tmpDir -Recurse -Force
+}
+$_ = mkdir $tmpDir -Force
+
+$vstest = "${env:ProgramFiles(X86)}\Microsoft Visual Studio\2017\Community\Common7\IDE\Extensions\TestPlatform\VSTest.Console.exe"
+if (!(Test-Path $vstest))
+{
+    Write-Warning "Did not find the Visual Studio 2017 Console Test Runner."
+    exit 1
+}
+
+$assemblies = @(
+    "$binDir\Mastersign.Tasks.Test.dll"
+)
+
+foreach ($assembly in $assemblies)
+{
+    if (!(Test-Path $assembly)) 
+    {
+        Write-Warning "Did not find the test assembly $assembly."
+    }
+}
+
+$errCount = 0
+$successCount = 0
+
+for ($i = 0; $i -lt $Cycles; $i++)
+{
+    foreach ($assembly in $assemblies)
+    {
+        $output = & $vstest $assembly "/TestAdapterPath:$binDir" "/TestCaseFilter:TestCategory!=Rendering"
+        $status = $LastExitCode
+        if ($status -ne 0)
+        {
+            $output | Out-File "$tmpDir\log_${i}.txt" -Encoding Default
+            $errCount++;
+            Write-Host "$i Failed"
+        }
+        else
+        {
+            $successCount++;
+            Write-Host "$i Passed"
+        }
+    }
+}
+
+if ($errCount -gt 0)
+{
+    Write-Warning "$errCount Failed, $successCount Passed"
+    exit 1
+}
+else
+{
+    Write-Host "$successCount Passed"
+    exit 0
+}

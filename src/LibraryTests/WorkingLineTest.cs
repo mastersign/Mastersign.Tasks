56d6c227abe97ee1bb9bd5f4ece1cef2274562ed
Tobias Kiertscher <kiertscher@th-brandenburg.de>
2017-10-10 14:49:42 +0200

    added cancellation tests to WorkingLineTest

--- src/LibraryTests/WorkingLineTest.cs
+++ src/LibraryTests/WorkingLineTest.cs
@@ -77,7 +77,7 @@ namespace Mastersign.Tasks.Test
             wl.Enqueue(task);
             Assert.IsFalse(q.IsEmpty);
             AssertState(wl, isDisposed: false, busy: false);
-            
+
             wl.Start();
             wl.WaitForEnd(timeout: 10000);
 
@@ -144,6 +144,91 @@ namespace Mastersign.Tasks.Test
                         TaskState.CleaningUp,
                         TaskState.Succeeded);
             }
+        }
+
+        [TestMethod]
+        public void SequentialCancellationTest()
+            => WithWorkingLine(SequentialCancellationTestCase, worker: 1, started: true);
+
+        private void SequentialCancellationTestCase(WorkingLine wl, EventMonitor<WorkingLine> wlMon)
+        {
+            var q = wl.Queue;
+            var tasks = Enumerable.Range(0, 4).Select(i => new TestTask(i.ToString())).ToArray();
+
+            var cancelTask = tasks[1];
+            cancelTask.StateChanging += (s, e) =>
+            {
+                if (e.NewValue == TaskState.InProgress) wl.Cancel();
+            };
+
+            wl.Enqueue(tasks);
+
+            Assert.IsTrue(wl.WaitForEnd(timeout: 4000));
+
+            AssertState(wl, isDisposed: false, busy: false);
+            Assert.AreEqual(0, wl.CurrentTasks.Length);
+            Assert.IsTrue(wl.Queue.IsEmpty);
+
+            Assert.AreEqual(TaskState.Succeeded, tasks[0].State);
+            Assert.AreEqual(TaskState.Canceled, tasks[1].State);
+            Assert.AreEqual(TaskState.Obsolete, tasks[2].State);
+            Assert.AreEqual(TaskState.Obsolete, tasks[3].State);
+
+            wlMon.History
+                .AssertSender(wl)
+                .AssertEventNames(
+                nameof(WorkingLine.BusyChanged),
+                nameof(WorkingLine.BusyWorkerCountChanged),
+                nameof(WorkingLine.TaskBegin),
+                nameof(WorkingLine.TaskEnd),
+                nameof(WorkingLine.TaskBegin),
+                nameof(WorkingLine.Cancelled),
+                nameof(WorkingLine.TaskEnd),
+                nameof(WorkingLine.BusyWorkerCountChanged),
+                nameof(WorkingLine.BusyChanged));
+        }
+
+        [TestMethod]
+        public void ParallelCancellationTest()
+            => WithWorkingLine(ParallelCancellationTestCase, worker: 3, started: true);
+
+        private void ParallelCancellationTestCase(WorkingLine wl, EventMonitor<WorkingLine> wlMon)
+        {
+            var q = wl.Queue;
+            var tasks = Enumerable.Range(0, 9).Select(i => new TestTask(i.ToString())).ToArray();
+            var taskMons = tasks.Select(t => new EventMonitor<TestTask>(t)).ToArray();
+
+            var cancelTask = tasks[5];
+            cancelTask.StateChanging += (s, e) =>
+            {
+                if (e.NewValue == TaskState.InProgress) wl.Cancel();
+            };
+
+            wl.Enqueue(tasks);
+
+            Assert.IsTrue(wl.WaitForEnd(timeout: 4000));
+
+            AssertState(wl, isDisposed: false, busy: false);
+            Assert.AreEqual(0, wl.CurrentTasks.Length);
+            Assert.IsTrue(wl.Queue.IsEmpty);
+
+            for (int i = 0; i < 3; i++) Assert.AreEqual(TaskState.Succeeded, tasks[i].State);
+            for (int i = 3; i < 6; i++) Assert.AreEqual(TaskState.Canceled, tasks[i].State);
+            for (int i = 6; i < 9; i++) Assert.AreEqual(TaskState.Obsolete, tasks[i].State);
+
+            wlMon.FilterHistory(ByEventName(
+                nameof(WorkingLine.BusyChanged),
+                nameof(WorkingLine.Cancelled)))
+                .AssertEventNames(
+                    nameof(WorkingLine.BusyChanged),
+                    nameof(WorkingLine.Cancelled),
+                    nameof(WorkingLine.BusyChanged));
+
+            wlMon.FilterHistory(ByEventName(nameof(WorkingLine.BusyChanged)))
+                .AssertPropertyValues(true, false);
+
+            wlMon.FilterHistory(ByEventName(nameof(WorkingLine.BusyWorkerCountChanged)))
+                .AssertPropertyValues(1, 2, 3, 2, 1, 0);
         }
     }
 }

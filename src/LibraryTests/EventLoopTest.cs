d461a35f16d8f21b83ae3ea618b891b3a975ecc0
Tobias Kiertscher <kiertscher@th-brandenburg.de>
2017-10-11 16:05:59 +0200

    added test to assure ordering of dispatch in EventLoop

---
+++ src/LibraryTests/EventLoopTest.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mastersign.Tasks.Test
+{
+    [TestClass]
+    public class EventLoopTest
+    {
+        [TestMethod]
+        public void ConcurrentOrderTest()
+        {
+            var eventLoop = new EventLoop();
+
+            var sourceItems = Enumerable.Range(0, 10000).ToArray();
+            var threadCount = 20;
+            var resultLists = Enumerable.Range(0, threadCount).Select(i => new List<int>()).ToArray();
+            var threadEvents = resultLists.Select(rl =>
+            {
+                var endEvent = new ManualResetEventSlim(false);
+                var t = new Thread(() =>
+                {
+                    var rand = new Random();
+                    foreach (var value in sourceItems) {
+                        var v = value;
+                        eventLoop.Push((Action)(() => { rl.Add(v); }));
+                        if (rand.NextDouble() > 0.9) Thread.Sleep(1);
+                    }
+                    endEvent.Set();
+                });
+                t.Start();
+                return endEvent;
+            }).ToArray();
+            threadEvents.All(te => { te.Wait(); te.Dispose(); return true; });
+            eventLoop.WaitForEmpty();
+
+            foreach (var rl in resultLists)
+            {
+                CollectionAssert.AreEqual(sourceItems, rl);
+            }
+        }
+    }
+}

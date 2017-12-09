addff58e5d4d036ab44324d6655130f47204ac1a
Tobias Kiertscher <dev@mastersign.de>
2017-12-09 14:49:18 +0100

    added TaskPropertyUpdateEventArgs

---
+++ src/Library/TaskPropertyUpdateEventArgs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mastersign.Tasks
+{
+    public class TaskPropertyUpdateEventArgs<T> : EventArgs
+    {
+        public ITask Task { get; private set; }
+
+        public string PropertyName { get; private set; }
+
+        public T OldValue { get; private set; }
+
+        public T NewValue { get; private set; }
+
+        public TaskPropertyUpdateEventArgs(ITask task, string propertyName, T oldValue, T newValue)
+        {
+            Task = task;
+            PropertyName = propertyName;
+            OldValue = oldValue;
+            NewValue = newValue;
+        }
+    }
+}
